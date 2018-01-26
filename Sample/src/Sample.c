ffea2954155eb72706ec5972e0edc25aa20800c6
Karthikeyan <electronkarthiece@gmail.com>
2018-01-26 20:12:24 +0530

    First commit

---
+++ Sample/src/Sample.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : Sample.c
+ Author      : Karthikeyan
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int a;
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	a = 10 +20;
+
+	return EXIT_SUCCESS;
+}
